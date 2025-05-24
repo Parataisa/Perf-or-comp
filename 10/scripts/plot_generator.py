7e7cf06f8da555a40b3aadaf26f22e721913e36f
Dominik Barbist <d.barbist@hotmail.com>
2025-05-24 18:14:21 +0200

    Fixed naming issues

--- 10/scripts/plot_generator.py
+++ 10/scripts/plot_generator.py
@@ -100,7 +100,7 @@ class PlotGenerator:
             unrolled_data = unrolled_data.copy()
             unrolled_data["chunk_size"] = (
                 unrolled_data["container"]
-                .str.extract(r"unrolled_(\d+)")
+                .str.extract(r"unrolled_linkedlist_(\d+)")
                 .astype(int)
             )
 
